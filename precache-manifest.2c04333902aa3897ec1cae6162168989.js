c167c110981749fa854145b2fd2d6e9fd1b7c37a
Jase <jasonlunn@live.co.uk>
2020-06-27 14:47:13 +0100

    Updates

---
+++ precache-manifest.2c04333902aa3897ec1cae6162168989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360bf8a094ed26b56cec57ad0e808331",
+    "url": "/react-personal-web-page/index.html"
+  },
+  {
+    "revision": "a232c6a5f3613803e5b3",
+    "url": "/react-personal-web-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9000894ba9afd0b9ed06",
+    "url": "/react-personal-web-page/static/js/2.8ba3c618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-personal-web-page/static/js/2.8ba3c618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a232c6a5f3613803e5b3",
+    "url": "/react-personal-web-page/static/js/main.206be987.chunk.js"
+  },
+  {
+    "revision": "d9ec0e658ac5ffc8d3ad",
+    "url": "/react-personal-web-page/static/js/runtime-main.bd764a0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-personal-web-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
