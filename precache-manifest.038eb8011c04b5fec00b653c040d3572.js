48a12c770f79a93a234e8cf77b72344a71ffd2c9
Jase <jasonlunn@live.co.uk>
2020-06-27 16:51:29 +0100

    Updates

---
+++ precache-manifest.038eb8011c04b5fec00b653c040d3572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34cd279ddf1549d02afdc79aab5cbb95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9801640271f37c109a50",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9000894ba9afd0b9ed06",
+    "url": "/static/js/2.8ba3c618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ba3c618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9801640271f37c109a50",
+    "url": "/static/js/main.7cfb4e3b.chunk.js"
+  },
+  {
+    "revision": "9456317067ffa5fca513",
+    "url": "/static/js/runtime-main.c0222d81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
